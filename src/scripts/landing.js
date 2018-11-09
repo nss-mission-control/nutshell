ffd5509f56cee76a8b40a38ed8600a07f83ec9e4
kellymorin <kmorin06@gmail.com>
2018-11-09 11:35:50 -0600

    Login page and event listener functionality.

--- src/scripts/landing.js
+++ src/scripts/landing.js
@@ -1,19 +1,20 @@
 import comp from "./components"
+import logInFuncs from "./login"
 
 const landingPageFuncs = {
   loadLandingPage() {
     let div2 = new comp.div(
       { classList: "welcome" },
       new comp.title("h1", { className: "title" }, "Welcome to Mission Control"),
-      new comp.btn({className: "btn"}, "Login"),
-      new comp.btn({className: "btn"}, "Register"))
+      new comp.btn("Login"),
+      new comp.btn("Register"))
     div2.render(".container--inner")
     let buttons = document.querySelectorAll("button")
 
     buttons.forEach((button) => {
       button.addEventListener("click", (e) => {
         if (e.target.textContent === "Login") {
-          console.log("login");
+          logInFuncs.loadLogIn()
         } else {
           console.log("register");
         }
