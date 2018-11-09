8b82f565a3cd320564894857692953c5ff03a309
Jase Hackman <42299515+jasehackman@users.noreply.github.com>
2018-11-09 13:25:35 -0600

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-import comp from "./components"
-import landingPageFuncs from "./landing"
-import buildMessages from "./messages"
-
-// landingPageFuncs.loadLandingPage();
-buildMessages.messageMap();
