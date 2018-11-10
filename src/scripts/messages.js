806193edaf5ab906c7b54e5fcd770492d46608ab
jasehackman <jasehackman@gmail.com>
2018-11-10 09:39:43 -0600

    messages post to json and update DOM

--- src/scripts/messages.js
+++ src/scripts/messages.js
@@ -1,6 +1,7 @@
 import comp from "./components"
 import API from "./apiData"
 
+const currentUser = 3;
 
 const buildMessages = {
   printMessages (messageObj) {
@@ -14,17 +15,41 @@ const buildMessages = {
     document.querySelector(".container--inner").innerHTML = ""
     API.getAllCategory("messages/?_expand=user")
     .then(messageObj => messageObj.forEach(message => {
-      console.log(message);
       this.printMessages(message)}))
-      .then(() => this.newMessage()) //check promise return on this line
+      .then(() => this.newMessage())
+        .then(() => this.submitMessage())//check promise return on this line
 
   },
 
   newMessage () {
-    const newMessageField = new comp.section ({className: "new--message"},
+    const newMessageField = new comp.section ({className: "new--message", id: "newMessage"},
     new comp.title ("h1", {}, "New Message"),
     new comp.textarea ({placeholder: "type your message here", wrap: "hard"}),
     new comp.btn ("Submit")).render(".container--inner")
+  },
+
+  submitMessage() {
+    $("#newMessage > button").click(function (e) {
+        e.preventDefault()
+       //creates object of current moment
+      let dateAndTime = new Date();
+      //converts it into a string and then an array to grab specific values
+      let dateArray = dateAndTime.toString().split(" ");
+      //getMonth() method returns a number between 0-11. Added 1 to get current month
+      let month = dateAndTime.getMonth()+1;
+      //builds object to pass into fetch
+      let submitMessageObj = {
+        messageContent: $("#newMessage > textarea").val(),
+        timeStamp: dateArray[4],
+        date: `${month}/${dateArray[2]}/${dateArray[3]}`,
+        userId: currentUser
+      }
+      console.log(submitMessageObj);
+      API.saveItem("messages", submitMessageObj)
+        .then(() => buildMessages.messageMap())
+
+
+    })
   }
 
 };
