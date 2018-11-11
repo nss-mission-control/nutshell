0a4f16a5765372acda948c58dcc110e88725e916
jasehackman <jasehackman@gmail.com>
2018-11-10 20:53:09 -0600

    form appears and can take you back to the page

--- src/scripts/events.js
+++ src/scripts/events.js
@@ -4,72 +4,110 @@ import API from "./apiData"
 let currentUser = 2;
 
 
-const buildTasks = {
+const buildEvents = {
 
-  buildContainers () {
+  buildContainers() {
     document.querySelector(".container--inner").innerHTML = ""
-    const newBtn = new comp.div ({id: "newEventBtn"},
-    new comp.btn ("+")).render(".container--inner")
+    const newBtn = new comp.div({
+        id: "newEventBtn"
+      },
+      new comp.btn("+")).render(".container--inner")
 
 
-    const title1 = new comp.title ("h1", {className: "title--upcoming"}, "Upcoming Event").render(".container--inner")
-    const incomplete = new comp.div ({id: "upcoming"}).render(".container--inner")
-    const title2 = new comp.title ("h1", {className: "title--past"}, "Past Event").render(".container--inner")
-    const complete = new comp.div ({id: "past"}).render(".container--inner")
+    const title1 = new comp.title("h1", {
+      className: "title--upcoming"
+    }, "Upcoming Event").render(".container--inner")
+    const incomplete = new comp.div({
+      id: "upcoming"
+    }).render(".container--inner")
+    const title2 = new comp.title("h1", {
+      className: "title--past"
+    }, "Past Event").render(".container--inner")
+    const complete = new comp.div({
+      id: "past"
+    }).render(".container--inner")
     // this.newTask()
     this.eventFetch();
     this.newEventButton();
   },
 
-  printEvents (eventObj) {
+  printEvents(eventObj) {
     let outputContainer;
 
     // need to test if date is in the future or the past
 
     outputContainer = "#upcoming"
-    const task = new comp.section ({className: "event", id: `${eventObj.id}`},
-    new comp.title("h3",  `${eventObj.name}`),
-    new comp.par(`${eventObj.date} ${eventObj.time}`),
-    new comp.par(`${eventObj.location}`)).render(outputContainer)
+    const task = new comp.section({
+        className: "event",
+        id: `${eventObj.id}`
+      },
+      new comp.title("h3", `${eventObj.name}`),
+      new comp.par(`${eventObj.date} ${eventObj.time}`),
+      new comp.par(`${eventObj.location}`)).render(outputContainer)
   },
 
-  eventFetch ()  {
+  eventFetch() {
     API.getAllCategory("events") //check if user is same as session storage
-    .then(eventObj =>  {
-      eventObj.forEach(event => {
-      this.printEvents(event)})
+      .then(eventObj => {
+        eventObj.forEach(event => {
+          this.printEvents(event)
+        })
 
-    })
+      })
   },
 
-  newEventButton () {
+  newEventButton() {
+    // when clicked it clears the dom and calls the function to build the form
     $("#newEventBtn").click(
       function (e) {
         console.log("click,click")
-        $(".container--inner").text(null)
-        buildTasks.newEventPopUp();
+        $(".container--inner").text("")
+        buildEvents.newEventPopUp();
 
       }
     )
   },
   newEventPopUp() {
-      // Builds new event entry form
-    let div2 = new comp.div(
-      { classList: "newEventForm" },
-      new comp.title("h1", { className: "title" }, "Add A New Event"),
+    // Builds new event entry form
+    let div2 = new comp.div({
+        classList: "newEventForm"
+      },
+      new comp.title("h1", {
+        className: "title"
+      }, "Add A New Event"),
       new comp.label("Event Name"),
-      new comp.input({type: "text"}),
+      new comp.input({
+        type: "text"
+      }),
       new comp.label("Date"),
-      new comp.input({type: "date"}),
+      new comp.input({
+        type: "date"
+      }),
       new comp.label("Time"),
-      new comp.input({type: "text"}),
+      new comp.input({
+        type: "text"
+      }),
       new comp.label("Location"),
-      new comp.input({type: "text"}),
+      new comp.input({
+        type: "text"
+      }),
       new comp.btn("Save"),
       new comp.btn("Back"))
     div2.render(".container--inner")
-    let buttons = document.querySelectorAll("button")
-    }
+    buildEvents.newEventPopUpBtnClicks();
+  },
+  newEventPopUpBtnClicks() {
+    // grabs the two buttons on the page and adds a click listener based on index
+    let popUpBtns = document.querySelectorAll("button");
+    popUpBtns[0].addEventListener("click", () => {
+      buildEvents.buildContainers();
+    })
+    popUpBtns[1].addEventListener("click", () => {
+      buildEvents.buildContainers();
+    })
+
+  }
+
 }
 
-export default buildTasks
+export default buildEvents
