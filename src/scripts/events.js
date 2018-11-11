d7d69d6596816c262615e53a18a03f11daa24c83
jasehackman <jasehackman@gmail.com>
2018-11-10 22:10:08 -0600

    edit buttons cause a form to appear. When submited the form does a PATCH on the api and repopulates the DOM

--- src/scripts/events.js
+++ src/scripts/events.js
@@ -1,37 +1,41 @@
 import comp from "./components"
 import API from "./apiData"
 
+// delete this
 let currentUser = 2;
 
 
 const buildEvents = {
 
   buildContainers() {
+    // builds the two containers to hold everything
     document.querySelector(".container--inner").innerHTML = ""
-    const newBtn = new comp.div({
-        id: "newEventBtn"
-      },
+    // button for new event
+    const newBtn = new comp.div({ id: "newEventBtn"},
+      new comp.title("h3", "New Event!"),
       new comp.btn("+")).render(".container--inner")
 
-
+    // containers
     const title1 = new comp.title("h1", {
       className: "title--upcoming"
     }, "Upcoming Event").render(".container--inner")
-    const incomplete = new comp.div({
+    const upcoming = new comp.div({
       id: "upcoming"
     }).render(".container--inner")
     const title2 = new comp.title("h1", {
       className: "title--past"
     }, "Past Event").render(".container--inner")
-    const complete = new comp.div({
+    const past = new comp.div({
       id: "past"
     }).render(".container--inner")
     // this.newTask()
-    this.eventFetch();
     this.newEventButton();
+    this.eventFetch()
+
   },
 
   printEvents(eventObj) {
+    // takes the objects from the api and prints them to the dom
     let outputContainer;
 
     // need to test if date is in the future or the past
@@ -43,7 +47,8 @@ const buildEvents = {
       },
       new comp.title("h3", `${eventObj.name}`),
       new comp.par(`${eventObj.date} ${eventObj.time}`),
-      new comp.par(`${eventObj.location}`)).render(outputContainer)
+      new comp.par(`${eventObj.location}`),
+      new comp.btn("Edit")).render(outputContainer)
   },
 
   eventFetch() {
@@ -52,7 +57,7 @@ const buildEvents = {
         eventObj.forEach(event => {
           this.printEvents(event)
         })
-
+        buildEvents.editBtnListen()
       })
   },
 
@@ -63,45 +68,37 @@ const buildEvents = {
         console.log("click,click")
         $(".container--inner").text("")
         buildEvents.newEventPopUp();
-
       }
     )
   },
+
   newEventPopUp() {
     // Builds new event entry form
     let div2 = new comp.div({
         classList: "newEventForm"
       },
-      new comp.title("h1", {
-        className: "title"
-      }, "Add A New Event"),
+      new comp.title("h1", { className: "title"}, "Add A New Event"),
       new comp.label("Event Name"),
-      new comp.input({
-        type: "text"
-      }),
+      new comp.input({ type: "text"}),
       new comp.label("Date"),
-      new comp.input({
-        type: "date"
-      }),
+      new comp.input({type: "date"}),
       new comp.label("Time"),
-      new comp.input({
-        type: "text"
-      }),
+      new comp.input({type: "text"}),
       new comp.label("Location"),
-      new comp.input({
-        type: "text"
-      }),
+      new comp.input({ type: "text"}),
       new comp.btn("Save"),
       new comp.btn("Back"))
     div2.render(".container--inner")
     buildEvents.newEventPopUpBtnClicks();
   },
+
   newEventPopUpBtnClicks() {
     // grabs the two buttons on the page and adds a click listener based on index
     const popUpBtns = document.querySelectorAll("button");
     popUpBtns[0].addEventListener("click", () => {
+      // Save Button
       const inputArray = document.querySelectorAll("input");
-      console.log(inputArray);
+      // builds object to send to api
       const newEventObj = {
         name: inputArray[0].value,
         date: inputArray[1].value,
@@ -109,7 +106,7 @@ const buildEvents = {
         location: inputArray[3].value,
         userId: currentUser
       }
-      console.log(newEventObj);
+      // saves new event to API
       API.saveItem("events", newEventObj).then(() => {
       buildEvents.buildContainers();
      }) })
@@ -118,8 +115,67 @@ const buildEvents = {
     popUpBtns[1].addEventListener("click", () => {
       buildEvents.buildContainers();
     })
+  },
+  editBtnListen () {
+    // listens for all the edit buttons on the page
+    const allTheButtons = document.querySelectorAll("section > button");
+    allTheButtons.forEach(currentBtn => {
+      currentBtn.addEventListener("click", () => {
+        // takes the id of the event that was clicks, fetches from the api with that id and passes on to the Edit Element form
+        const currentBtnId = currentBtn.parentElement.id;
+        API.getOneFromCategory("events", currentBtnId)
+          .then(singleEvent => {
+            $(".container--inner").text("")
+            buildEvents.eventEditForm(singleEvent, currentBtnId)
+          })
+      })
+    })
+  },
+  eventEditForm(singleEventObj) {
+    // builds Edit form
+    // takes the return from the fetch
+    let div2 = new comp.div({
+      classList: "newEventForm"
+    },
+    new comp.title("h1", { className: "title"}, "Edit Your Event"),
+    new comp.label("Event Name"),
+    new comp.input({ type: "text", value: `${singleEventObj.name}`}),
+    new comp.label("Date"),
+    new comp.input({type: "date", value: `${singleEventObj.date}`}),
+    new comp.label("Time"),
+    new comp.input({type: "text", value: `${singleEventObj.time}`}),
+    new comp.label("Location"),
+    new comp.input({ type: "text", value: `${singleEventObj.location}`}),
+    new comp.btn("Save"),
+    new comp.btn("Back"))
+  div2.render(".container--inner")
+  buildEvents.editEventPopUpBtnClicks(singleEventObj.id);
+  },
+  editEventPopUpBtnClicks(id) {
+    // grabs the two buttons on the page and adds a click listener based on index
+    // takes the event id so it can be passed on with the PATCH
+    const popUpBtns = document.querySelectorAll("button");
+    popUpBtns[0].addEventListener("click", () => {
+      // Save Button
+      const inputArray = document.querySelectorAll("input");
+      // builds object to send to api
+      const editEventObj = {
+        name: inputArray[0].value,
+        date: inputArray[1].value,
+        time: inputArray[2].value,
+        location: inputArray[3].value,
+        userId: currentUser
+      }
+      // saves new event to API
+      API.updateItem("events", id, editEventObj).then(() => {
+      buildEvents.buildContainers();
+     }) })
 
-  }
+    // Back Button Returns to Event Page
+    popUpBtns[1].addEventListener("click", () => {
+      buildEvents.buildContainers();
+    })
+  },
 
 }
 
