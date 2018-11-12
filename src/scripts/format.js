9e96b1c6f7458708bf17efd8c558128f4209f00e
kellymorin <kmorin06@gmail.com>
2018-11-12 15:07:25 -0600

    updated date and time formatting to account for local time zone.

--- src/scripts/format.js
+++ src/scripts/format.js
@@ -7,21 +7,35 @@ const formatDate = {
     return dateFormatted;
   },
 
-  getCorrectTime(eventTime){
+  correctDateAndTime(eventTime){
+    //Take timestamp and capture first half of string containing date, split into an array on the - element
+    let datearr =`${eventTime}`.substring(0,10).split("-");
+    //Take timestamp and capture second half of string containing time, split into array on the : element
     let timeArr = `${eventTime}`.substring(11,16).split(":");
     let morningEvening= "am";
+    //Subtract 6 hours to account for local time
+    timeArr[0] = (timeArr[0]-6)
+    //If hour is greater than 12, reformat for pm
     if(timeArr[0] > 12){
       timeArr[0] = (timeArr[0]-12);
       morningEvening = "pm";
+      //If hour is 0, set to midnight
     } else if(timeArr[0] === 0){
       timeArr[0] = 12;
-    }else{
-      timeArr[0] = timeArr[0].substring(1);
+      //If hour is less than 0, reformat to display previous day and corrected time
+    } else if(timeArr[0]< 0){
+      timeArr[0] = (24 + timeArr[0]);
+      if(timeArr[0]> 12){
+        timeArr[0] = (timeArr[0]-12)
+        morningEvening = "pm"
+        datearr[2] = (datearr[2]-1)
+      }
     }
+    [datearr[0], datearr[1], datearr[2]] = [datearr[1], datearr[2], datearr[0]];
     let updatedTime = timeArr.join(":");
     timeArr = [];
     timeArr.unshift(updatedTime, morningEvening);
-    let timeFormatted = timeArr.join(" ");
+    let timeFormatted = `${datearr.join("/")} ${timeArr.join(" ")} `;
     return timeFormatted;
   }
 }
