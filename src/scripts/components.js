f3dc2bc1a1837d12c5f542b1ddf6176b56c375f4
kellymorin <kmorin06@gmail.com>
2018-11-12 13:26:23 -0600

    Working on reformatting date and time functionality.

--- src/scripts/components.js
+++ src/scripts/components.js
@@ -120,3 +120,5 @@ export default Object.create(null, {
     }
   }
 })
+
+
---
+++ src/scripts/format.js
@@ -0,0 +1,28 @@
+
+
+export default Object.create(null, {
+  getCorrectDate (eventDate){
+    let datearr =`${eventDate}`.substring(0,10).split("-");
+    [datearr[0], datearr[1], datearr[2]] = [datearr[1], datearr[2], datearr[0]];
+    dateFormatted = datearr.join("/");
+    return dateFormatted;
+  },
+
+  getCorrectTime(eventTime){
+    let timeArr = `${eventTime}`.substring(11,16).split(":");
+    let morningEvening= "am";
+    if(timeArr[0] > 12){
+      timeArr[0] = (timeArr[0]-12);
+      morningEvening = "pm";
+    } else if(timeArr[0] === 0){
+      timeArr[0] = 12;
+    }else{
+      timeArr[0] = timeArr[0].substring(1);
+    }
+    updatedTime = timeArr.join(":");
+    timeArr = [];
+    timeArr.unshift(updatedTime, morningEvening);
+    timeFormatted = timeArr.join(" ");
+    return timeFormatted;
+  }
+})
