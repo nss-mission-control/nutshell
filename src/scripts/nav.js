67d9a40ac4ccedfa06d227cfbf5947dfb4d343f9
braddavistech <brad@braddavistech.com>
2018-11-12 17:32:11 -0600

    added stylings

--- src/scripts/nav.js
+++ src/scripts/nav.js
@@ -14,12 +14,6 @@ const navBar = {
     if (sessionStorage.getItem("currentUser") === null) {
       new comp.ul(
         {},
-        // new comp.li({}, "Home"),
-        // new comp.li({}, "Tasks"),
-        // new comp.li({}, "Events"),
-        // new comp.li({}, "Messages"),
-        // new comp.li({}, "News"),
-        // new comp.li({}, "Friends"),
         new comp.li({}, "Log In")
       ).render("#navBar")
     } else {
@@ -53,51 +47,18 @@ const navBar = {
 
     document.querySelector("#navBar").addEventListener("click", (event) => {
       if (event.target.textContent === "Home") {
-        // if (sessionStorage.getItem("currentUser") === null) {
-        //   console.log("Not logged in.");
-        //   logInFuncs.loadLogIn();
-        // } else {
           buildMissionControl.printPlaceholder();
-        // }
       } else if (event.target.textContent === "Tasks") {
-        // if (sessionStorage.getItem("currentUser") === null) {
-        //   console.log("Not logged in.");
-        //   logInFuncs.loadLogIn();
-        // } else {
           buildTasks.buildContainers();
-        // }
       } else if (event.target.textContent === "Events") {
-        // if (sessionStorage.getItem("currentUser") === null) {
-        //   console.log("Not logged in.");
-        //   logInFuncs.loadLogIn();
-        // } else {
           buildEvents.buildContainers()
-        // }
       } else if (event.target.textContent === "Messages") {
-        // if (sessionStorage.getItem("currentUser") === null) {
-        //   console.log("Not logged in.");
-        //   logInFuncs.loadLogIn();
-        // } else {
           buildMessages.messageMap();
-        // }
       } else if (event.target.textContent === "News") {
-        // if (sessionStorage.getItem("currentUser") === null) {
-        //   console.log("Not logged in.");
-        //   logInFuncs.loadLogIn();
-        // } else {
           buildNews.newsMap();
-        // }
       } else if (event.target.textContent === "Friends") {
-        // if (sessionStorage.getItem("currentUser") === null) {
-        //   console.log("Not logged in.");
-        //   logInFuncs.loadLogIn();
-        // } else {
           console.log("Friends function calles.")
-        // }
       }
-      // else if (event.target.textContent === "Log In") {
-      //   logInFuncs.loadLogIn()
-      // }
     })
   },
 
