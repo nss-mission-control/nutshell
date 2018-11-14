e903b58910d78d59f465481c2a3ea58830a4fe16
kellymorin <kmorin06@gmail.com>
2018-11-14 08:15:04 -0600

    Completed form verification and some styling changes to ensure the articles clear the add new article form at the top.

--- src/scripts/news.js
+++ src/scripts/news.js
@@ -5,24 +5,30 @@ import formatDate from "./format"
 
 
 const buildNews = {
+  //Build the containers on the news page to hold the articles
   createContainer(){
     new comp.section({className:"new--news"},
       new comp.btn("+"),
       new comp.title("h2", {}, "Save New Article")
     ).render(".container--inner")
+    new comp.section({className: "display--news"}).render(".container--inner")
   },
+  //Create each section used to display the article in the DOM
   printNews(newsObj) {
     new comp.section ({className: "news", id: `${newsObj.id}`},
     new comp.anchor({href: `${newsObj.url}`, target: "_blank"},  new comp.image({src: `${newsObj.articleImage}`, alt: "Article Image", height: "120", width: "120"})),
+    new comp.div({className: "news-info"},
     new comp.title("h2", {}, `${newsObj.articleName}`),
     new comp.title("h4", {}, `Saved by: ${newsObj.user.firstName} | Date Saved: ${formatDate.getCorrectDate(newsObj.dateSaved)}`),
-    new comp.par({}, newsObj.about),
-    new comp.btn("Delete Article")).render(".container--inner")
+    new comp.par({}, newsObj.about)),
+    new comp.btn("Delete Article")).render(".display--news")
   },
 
+  //Call the API, get all relevant news articles, loop over them and create the section to be displayed in the DOM, then attach the event listeners
   newsMap ()  {
     document.querySelector(".container--inner").innerHTML = ""
     this.createContainer()
+    document.querySelector(".display--news").innerHTML = ""
     API.getAllCategory(`articles/?userId=${activeUser.info().id}&_expand=user&_sort=dateSaved&_order=desc`)
     .then(newsObj => newsObj.forEach(news => {
       this.printNews(news)}))
@@ -30,7 +36,9 @@ const buildNews = {
 
   },
 
+  //builds the form to add a new article and calls the event listener function
   newNews () {
+      new comp.div({id: "alert"}).render(".new--news")
       new comp.input({name: "articleName", placeholder: "Article Name", id: "articleName" }).render(".new--news")
       new comp.input({name: "articleUrl", placeholder: "Article Link", id: "articleLink"}).render(".new--news")
       new comp.input({name: "articleImageUrl", placeholder: "Article Image Link", id: "articleImage"}).render(".new--news")
@@ -41,12 +49,21 @@ const buildNews = {
 
   eventListener(){
     document.querySelectorAll("button").forEach((button)=> {
+      //Grabs all buttons on the page and adds an event listener to them
       button.addEventListener("click", (e)=>{
+        //if button is "+", call the function to build the new article form
         if(e.target.textContent === "+"){
           this.newNews()
           $(".new--news button:first-child").remove()
         }
+        //if the button is "Save New Article", select the inputs and check to see if any field is blank, if so, display an alert
         else if(e.target.textContent === "Save New Article"){
+          let inputArray = document.querySelectorAll("input");
+          if(inputArray[0].value === "" || inputArray[1].value === "" || inputArray[2].value === "" || inputArray[3].value === ""){
+            new comp.par({className: "alert"}, "All fields are required.").render("#alert");
+            return;
+          }
+          //if the fields are not blank, create the object to pass to the API
           let story = {
             articleName: document.querySelector("#articleName").value,
             url: document.querySelector("#articleLink").value,
@@ -55,7 +72,9 @@ const buildNews = {
             userId: activeUser.info().id,
             dateSaved: new Date()
           }
+          //Call the API and pass the object created above
           buildNews.addNews(story)
+          //If the button is "Delete", select the parent node ID and call the API delete function, then re-render the articles section.
         } else if(e.target.textContent === "Delete Article"){
           let articleId = e.target.parentNode.id
           API.deleteItem("articles", articleId).then(()=> buildNews.newsMap())
