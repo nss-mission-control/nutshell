fcff8665b132f09b1449e0df63d57836542a43ae
braddavistech <brad@braddavistech.com>
2018-11-15 07:09:02 -0600

    added comments to apiData.js file

--- src/scripts/apiData.js
+++ src/scripts/apiData.js
@@ -4,16 +4,20 @@
 const URL = "http://localhost:8088/"
 
 const API = {
+
+  // api call to return all results in a given object
   getAllCategory(category) {
     return fetch(`${URL}${category}`)
       .then(entries => entries.json())
   },
 
+  // api call to return results for individual id
   getOneFromCategory(category, id) {
     return fetch(`${URL}${category}/${id}`)
       .then(inputs => inputs.json())
   },
 
+  // api call to save new user, message, friend, task, news article, etc
   saveItem(category, item) {
     return fetch(`${URL}${category}`, {
       method: "POST",
@@ -25,6 +29,7 @@ const API = {
     ).then(jsonData => jsonData.json())
   },
 
+  // api call to delete object from json file
   deleteItem(category, id) {
     return fetch(`${URL}${category}/${id}`, {
       method: "DELETE",
@@ -34,6 +39,7 @@ const API = {
     })
   },
 
+  //api call to edit current object
   updateItem(category, id, item){
     return fetch(`${URL}${category}/${id}`, {
       method: "PATCH",
