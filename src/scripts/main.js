47ca6e2a5bc800dab8e42cff34fe9a4da30f15b6
braddavistech <brad@braddavistech.com>
2018-11-15 07:15:19 -0600

    added comments to main.js file

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,6 +6,8 @@ import navBar from "./nav";
 import buildMissionControl from "./missionControl";
 
 navBar.loadNavBar();
+// checks if there is currently a user logged in and either loads mission control or login
+// this functionality only exists if the login and register new user password inputs are not set to type: password
 if (sessionStorage.getItem("currentUser") === null) {
   landingPageFuncs.loadLandingPage()
 } else {
