8c83aba671bd7437e8ba2da3ef3f4321d6fa67c5
braddavistech <brad@braddavistech.com>
2018-11-15 07:27:14 -0600

    added comments to sessionStorage.js file

--- src/scripts/sessionStorage.js
+++ src/scripts/sessionStorage.js
@@ -2,6 +2,9 @@
 // Purpose: Incorporation of sessionStorage to a call with json parsing due to object storage
 
 const activeUser = {
+
+  // grabs info from session storage and returns parsed data
+  // in future, we will only store an id, not an object
   info () {
     let loggedInUser = JSON.parse(sessionStorage.currentUser);
       return loggedInUser;
